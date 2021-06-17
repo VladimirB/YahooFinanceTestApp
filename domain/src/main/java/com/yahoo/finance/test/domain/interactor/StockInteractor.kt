a19b748ffbb21e4f984552d12fb6aad2726dcfc1
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-17 22:46:32 +0500

    Add the use-cases to obtain a stock list and the details

--- domain/src/main/java/com/yahoo/finance/test/domain/interactor/StockInteractor.kt
+++ domain/src/main/java/com/yahoo/finance/test/domain/interactor/StockInteractor.kt
@@ -1,4 +1,34 @@
 package com.yahoo.finance.test.domain.interactor
 
-class StockInteractor {
+import com.yahoo.finance.test.domain.model.Stock
+import com.yahoo.finance.test.domain.repository.StockRepository
+import com.yahoo.finance.test.domain.scheduler.SchedulerProvider
+import io.reactivex.Single
+import javax.inject.Inject
+
+class StockInteractor
+@Inject constructor(
+    private val stockRepository: StockRepository,
+    schedulerProvider: SchedulerProvider
+) : Interactor(schedulerProvider) {
+
+    fun getStocks(tickers: List<String>): Single<List<Stock>> {
+        return if (tickers.isEmpty()) {
+            Single.just(listOf())
+        } else {
+            stockRepository.getStocks(tickers)
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+        }
+    }
+
+    fun getStockDetails(ticker: String): Single<Stock> {
+        return if (ticker.isEmpty()) {
+            Single.error(IllegalStateException("Please, provide a ticker"))
+        } else {
+            stockRepository.getStockDetails(ticker)
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+        }
+    }
 }
