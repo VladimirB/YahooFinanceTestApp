13a6ee82a47d1a69b19fc95f1d6cfaec2e061ee8
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-17 22:14:55 +0500

    Add Stock model
    This model represents an abstraction of the stock exchange information about any ticker.

---
+++ domain/src/main/java/com/yahoo/finance/test/domain/Stock.kt
@@ -0,0 +1,18 @@
+package com.yahoo.finance.test.domain
+
+data class Stock(
+    val symbol: String,
+    val price: Double,
+    val change: Double,
+    val changePercent: Double,
+    val changePercentString: String,
+    val high: Double,
+    val low: Double,
+    val previousClose: Double,
+    val open: Double,
+    val dateTime: Long,
+    val currency: String,
+    val tickerShortName: String? = null,
+    val tickerLongName: String? = null,
+    val exchangeName: String? = null
+)
