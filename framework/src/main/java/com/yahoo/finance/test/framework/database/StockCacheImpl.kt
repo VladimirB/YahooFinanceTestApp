d52a125b9950cbe6c7b4bbb907976792d9c8cb0a
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 09:33:39 +0500

    Add StockCache implementation

---
+++ framework/src/main/java/com/yahoo/finance/test/framework/database/StockCacheImpl.kt
@@ -0,0 +1,27 @@
+package com.yahoo.finance.test.framework.database
+
+import com.yahoo.finance.test.data.database.StockCache
+import com.yahoo.finance.test.domain.model.Stock
+import io.reactivex.Maybe
+import io.reactivex.Single
+import javax.inject.Inject
+
+class StockCacheImpl
+@Inject constructor(private val dao: StockDao) : StockCache {
+
+    override fun getAll(): Maybe<List<Stock>> {
+        return dao.getAll()
+            .flatMapMaybe { cachedStocks ->
+                if (cachedStocks.isEmpty()) {
+                    Maybe.empty()
+                } else {
+                    val stocks = cachedStocks.map { it.toStock() }
+                    Maybe.just(stocks)
+                }
+            }
+    }
+
+    override fun save(stock: Stock): Single<Long> {
+        return dao.insert(stock.toStockLocal())
+    }
+}
