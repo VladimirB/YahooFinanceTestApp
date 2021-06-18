647753a5e549ee244ff3efb00da91a75c989e972
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 11:15:07 +0500

    Add StockRecyclerAdapter initialization for MainActivity

--- app/src/main/java/com/yahoo/finance/test/app/MainActivity.kt
+++ app/src/main/java/com/yahoo/finance/test/app/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.yahoo.finance.test.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.yahoo.finance.test.app.databinding.ActivityMainBinding
+import com.yahoo.finance.test.app.ui.StockRecyclerViewAdapter
+import com.yahoo.finance.test.domain.model.Stock
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var stockAdapter: StockRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
+        stockAdapter = StockRecyclerViewAdapter { stock -> onStockClicked(stock) }
+        binding.recyclerView.apply {
+            adapter = stockAdapter
+            itemAnimator = DefaultItemAnimator()
+            layoutManager = LinearLayoutManager(context)
+            val divider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+            addItemDecoration(divider)
+        }
+    }
+
+    private fun onStockClicked(stock: Stock) {
+
     }
 }
