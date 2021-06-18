709c93caea2ad18ee414c9ce8c5074cc96d290c3
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 10:22:54 +0500

    Add recycler adapter for Stock items

--- app/src/main/java/com/yahoo/finance/test/app/ui/StockRecyclerViewAdapter.kt
+++ app/src/main/java/com/yahoo/finance/test/app/ui/StockRecyclerViewAdapter.kt
@@ -1,4 +1,69 @@
 package com.yahoo.finance.test.app.ui
 
-class StockRecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.yahoo.finance.test.app.R
+import com.yahoo.finance.test.app.databinding.RecyclerItemStockBinding
+import com.yahoo.finance.test.domain.model.Stock
+import java.text.NumberFormat
+
+private val numberFormat = NumberFormat.getNumberInstance().apply {
+    maximumFractionDigits = 2
+    minimumFractionDigits = 2
+}
+
+class StockRecyclerViewAdapter(
+    private val onClickListener: ((Stock) -> Unit)? = null
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private class ViewHolder(
+        private val binding: RecyclerItemStockBinding,
+        private val onClickListener: ((Stock) -> Unit)? = null
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(stock: Stock) {
+            binding.stockName.text = stock.symbol
+            binding.companyName.text = stock.tickerShortName
+            binding.price.text = numberFormat.format(stock.price)
+
+            val change = numberFormat.format(stock.change) + " (" + stock.changePercentString + ")"
+            binding.change.text = change
+            val context = binding.root.context
+            if (stock.change >= 0) {
+                binding.change.setTextColor(ContextCompat.getColor(context, R.color.positiveTextColor))
+            } else {
+                binding.change.setTextColor(ContextCompat.getColor(context, R.color.negativeTextColor))
+            }
+
+            binding.root.setOnClickListener {
+                onClickListener?.invoke(stock)
+            }
+        }
+    }
+
+    private var stocks = listOf<Stock>()
+
+    fun setItems(stocks: List<Stock>) {
+        this.stocks = stocks
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding: RecyclerItemStockBinding = RecyclerItemStockBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ViewHolder).bind(stocks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return stocks.size
+    }
 }
