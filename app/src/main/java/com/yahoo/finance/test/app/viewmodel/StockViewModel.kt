569ea8e01274723752b8990a0c8bc07122de372f
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 12:47:14 +0500

    Add loadStockDetails() method at StockViewModel

--- app/src/main/java/com/yahoo/finance/test/app/viewmodel/StockViewModel.kt
+++ app/src/main/java/com/yahoo/finance/test/app/viewmodel/StockViewModel.kt
@@ -15,6 +15,9 @@ class StockViewModel : BaseViewModel() {
     private val _stocks: MutableLiveData<Event<List<Stock>>> = MutableLiveData()
     val stocks: LiveData<Event<List<Stock>>> = _stocks
 
+    private val _stockDetails: MutableLiveData<Event<Stock>> = MutableLiveData()
+    val stockDetails: LiveData<Event<Stock>> = _stockDetails
+
     fun loadStocks(tickers: List<String>) {
         disposables.add(stockInteractor.getStocks(tickers)
             .doOnSubscribe { _stocks.value = Event.Loading() }
@@ -22,6 +25,17 @@ class StockViewModel : BaseViewModel() {
                 _stocks.value = Event.Success(it)
             }, {
                 _stocks.value = Event.Failure(it)
+            })
+        )
+    }
+
+    fun loadStockDetails(ticker: String) {
+        disposables.add(stockInteractor.getStockDetails(ticker)
+            .doOnSubscribe { _stockDetails.value = Event.Loading() }
+            .subscribe({
+                _stockDetails.value = Event.Success(it)
+            }, {
+                _stockDetails.value = Event.Failure(it)
             })
         )
     }
