b363dc968366983c550a70d628237311b976d354
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 11:42:47 +0500

    Add SchedulerProviderModule to AppComponent

--- app/src/main/java/com/yahoo/finance/test/app/di/AppComponent.kt
+++ app/src/main/java/com/yahoo/finance/test/app/di/AppComponent.kt
@@ -5,15 +5,19 @@ import com.yahoo.finance.test.data.di.RepositoryModule
 import com.yahoo.finance.test.domain.di.InteractorModule
 import com.yahoo.finance.test.framework.di.DatabaseModule
 import com.yahoo.finance.test.framework.di.NetworkModule
+import com.yahoo.finance.test.framework.di.SchedulerProviderModule
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(
     modules = [
         AppModule::class,
         InteractorModule::class,
         RepositoryModule::class,
         NetworkModule::class,
-        DatabaseModule::class]
+        DatabaseModule::class,
+        SchedulerProviderModule::class]
 )
 interface AppComponent {
     fun inject(viewModel: StockViewModel)
