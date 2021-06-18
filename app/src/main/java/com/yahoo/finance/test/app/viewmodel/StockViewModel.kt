35e9a35794c895d41983dcc25318a57751a7a8da
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 11:26:43 +0500

    Add loadStock method at StockViewModel

--- app/src/main/java/com/yahoo/finance/test/app/viewmodel/StockViewModel.kt
+++ app/src/main/java/com/yahoo/finance/test/app/viewmodel/StockViewModel.kt
@@ -1,4 +1,28 @@
 package com.yahoo.finance.test.app.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.yahoo.finance.test.app.model.Event
+import com.yahoo.finance.test.domain.interactor.StockInteractor
+import com.yahoo.finance.test.domain.model.Stock
+import javax.inject.Inject
+
 class StockViewModel : BaseViewModel() {
+
+    @Inject
+    lateinit var stockInteractor: StockInteractor
+
+    private val _stocks: MutableLiveData<Event<List<Stock>>> = MutableLiveData()
+    val stocks: LiveData<Event<List<Stock>>> = _stocks
+
+    fun loadStocks(tickers: List<String>) {
+        disposables.add(stockInteractor.getStocks(tickers)
+            .doOnSubscribe { _stocks.value = Event.Loading() }
+            .subscribe({
+                _stocks.value = Event.Success(it)
+            }, {
+                _stocks.value = Event.Failure(it)
+            })
+        )
+    }
 }
