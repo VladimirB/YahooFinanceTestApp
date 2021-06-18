490b854162ce495e39c9cfe678ae198bc8a3b545
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 12:59:15 +0500

    Add getByTicker() method at StockDao class

--- framework/src/main/java/com/yahoo/finance/test/framework/database/StockDao.kt
+++ framework/src/main/java/com/yahoo/finance/test/framework/database/StockDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import io.reactivex.Maybe
 import io.reactivex.Single
 
 @Dao
@@ -11,6 +12,9 @@ interface StockDao {
 
     @Query("SELECT * FROM stocks")
     fun getAll(): Single<List<StockLocal>>
+
+    @Query("SELECT * FROM stocks WHERE ticker = :ticker")
+    fun getByTicker(ticker: String): Maybe<StockLocal>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(stockLocal: StockLocal): Single<Long>
