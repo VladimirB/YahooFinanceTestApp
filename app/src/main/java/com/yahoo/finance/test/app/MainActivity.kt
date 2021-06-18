e03f52540be4a4d4219f7fa24c3b678003d1d48b
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 11:36:22 +0500

    Add StockViewModel initialization and the list of the test tickers

--- app/src/main/java/com/yahoo/finance/test/app/MainActivity.kt
+++ app/src/main/java/com/yahoo/finance/test/app/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.yahoo.finance.test.app
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yahoo.finance.test.app.databinding.ActivityMainBinding
 import com.yahoo.finance.test.app.di.Injector
+import com.yahoo.finance.test.app.model.Event
 import com.yahoo.finance.test.app.ui.StockRecyclerViewAdapter
+import com.yahoo.finance.test.app.viewmodel.StockViewModel
 import com.yahoo.finance.test.domain.model.Stock
+
+private val testTickers = listOf("GOOG", "MSFT", "TWTR", "NVDA", "PYPL")
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var viewModel: StockViewModel
 
     private lateinit var stockAdapter: StockRecyclerViewAdapter
 
@@ -22,7 +31,28 @@ class MainActivity : AppCompatActivity() {
 
         Injector.install(this)
 
+        initializeViewModel()
         initializeRecyclerView()
+    }
+
+    private fun initializeViewModel() {
+        viewModel = ViewModelProvider(this)[StockViewModel::class.java]
+        viewModel.stocks.observe(this, { event ->
+            val visibility = if (event is Event.Loading) View.VISIBLE else View.GONE
+            binding.progressBar.visibility = visibility
+
+            when (event) {
+                is Event.Success -> {
+                    event.data?.let { stockAdapter.setItems(it) }
+                }
+                is Event.Failure -> {
+                    Toast.makeText(this, event.error?.message, Toast.LENGTH_SHORT).show()
+                }
+                else -> {}
+            }
+        })
+
+        viewModel.loadStocks(testTickers)
     }
 
     private fun initializeRecyclerView() {
