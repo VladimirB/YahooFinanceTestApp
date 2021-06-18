de039eee21811ed211fd4caa1c57854cf51d751a
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 12:37:10 +0500

    Add start() method for StockDetailsActivity with the Ticker parameter

--- app/src/main/java/com/yahoo/finance/test/app/ui/StockDetailsActivity.kt
+++ app/src/main/java/com/yahoo/finance/test/app/ui/StockDetailsActivity.kt
@@ -1,9 +1,22 @@
 package com.yahoo.finance.test.app.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class StockDetailsActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val EXTRA_TICKER = "extra_ticker"
+
+        fun start(context: Context, ticker: String) {
+            val intent = Intent(context, StockDetailsActivity::class.java)
+            intent.putExtra(EXTRA_TICKER, ticker)
+            context.startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
