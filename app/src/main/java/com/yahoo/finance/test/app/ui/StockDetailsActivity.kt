d7cc9fc51690b3072e78eab1c657e9c9a3daacf7
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 12:43:08 +0500

    Add StockDetailsActivity title and back button handler

--- app/src/main/java/com/yahoo/finance/test/app/ui/StockDetailsActivity.kt
+++ app/src/main/java/com/yahoo/finance/test/app/ui/StockDetailsActivity.kt
@@ -3,7 +3,9 @@ package com.yahoo.finance.test.app.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.yahoo.finance.test.app.databinding.ActivityStockDetailsBinding
 
 class StockDetailsActivity : AppCompatActivity() {
 
@@ -18,7 +20,23 @@ class StockDetailsActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var binding: ActivityStockDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityStockDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val ticker = intent.getStringExtra(EXTRA_TICKER)
+        supportActionBar?.title = ticker
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
