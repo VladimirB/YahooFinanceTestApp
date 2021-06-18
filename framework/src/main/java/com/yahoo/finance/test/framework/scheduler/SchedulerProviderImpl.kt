f1c80e3af098d953488cb7852ba5621af6f763b6
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2021-06-18 11:38:29 +0500

    Add SchedulerProvider implementation on Framework layer

---
+++ framework/src/main/java/com/yahoo/finance/test/framework/scheduler/SchedulerProviderImpl.kt
@@ -0,0 +1,19 @@
+package com.yahoo.finance.test.framework.scheduler
+
+import com.yahoo.finance.test.domain.scheduler.SchedulerProvider
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+class SchedulerProviderImpl
+@Inject constructor() : SchedulerProvider {
+    
+    override fun io(): Scheduler {
+        return Schedulers.io()
+    }
+
+    override fun ui(): Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+}
